eb73f7459fadd9e024eb33ba3c8bdad18a9e4aa1
Richard Young <ry@richyoung.ca>
2019-07-14 23:00:30 -0400

    Scripts to setup with podman

---
+++ build.sh
@@ -0,0 +1,1 @@
+podman build  --volume "/mnt/BUILDDATA/yocto/warrior-crops-work:/workdir" -t ry-poky   .
---
+++ run.sh
@@ -0,0 +1,17 @@
+
+user_id_real=$(id -u)
+max_uid_count=65536
+max_minus_uid=$((max_uid_count - user_id_real))
+uid_plus_one=$((user_id_real + 1))
+
+#--userns=keep-id 
+
+#podman run --rm --privileged  --volume "/mnt/BUILDDATA/yocto/warrior-crops-work:/workdir" -ti ry-poky   --workdir=/workdir 
+
+podman run --rm --privileged  -uidmap "$user_id_real":0:1  --uidmap 0:1:"$user_id_real" --uidmap "$uid_plus_one":"$uid_plus_one":"$max_minus_uid"  --volume "/mnt/BUILDDATA/yocto/warrior-crops-work:/workdir" -ti ry-poky   --workdir=/workdir 
+
+#--cmd chown -R 1000:1000 /workdir
+
+#podman run --rm --volume /mnt/BUILDDATA/yocto/warrior-crops:/workdir    -it ry-poky  --workdir=/workdir
+
+#podman run --rm -it -v /mnt/BUILDDATA/yocto/warrior-crops:/workdir localhost/ry-poky --workdir=/workdir
