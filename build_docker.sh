6352dec39144f71177f60fdd1de3f541b3cfca94
Richard Young <ry@richyoung.ca>
2019-09-22 15:33:42 -0400

    build scripts

---
+++ build_docker.sh
@@ -0,0 +1,9 @@
+
+#podman build  -t ry-poky   .
+#docker --env HTTP_PROXY="http://192.168.133.1:3128" --env HTTPS_PROXY="http://192.168.133.1:3128" build --volume "/home/ry/git_yocto/rnd-poky:/workdir" -t ry-poky   .
+
+
+docker build -f Dockerfile_fedora --volume "/home/ry/git_yocto/rnd-poky:/workdir" -t ry-poky-crops-fedora30 .  
+
+docker build  --volume "/home/ry/git_yocto/rnd-poky:/workdir" -t ry-poky   .
+
---
+++ build_podman.sh
@@ -0,0 +1,3 @@
+
+#podman build  -t ry-poky   .
+podman build  --volume "/home/ry/git_yocto/rnd-poky:/workdir"   -t ry-poky   .
