1e9b33829d79b4a979acbb1715d257607001c2c7
Richard Young <ry@richyoung.ca>
2019-11-26 01:19:01 -0500

    Working podman of fedora build

--- build_podman.sh
+++ build_podman.sh
@@ -1,3 +1,6 @@
 
 #podman build  -t ry-poky   .
-podman build  --volume "/home/ry/git_yocto/rnd-poky:/workdir"   -t ry-poky   .
+#-t ry-crops-fedora30
+
+podman build -f Dockerfile_fedora --volume "/home/ry/git_yocto/rnd-poky:/workdir"   -t ry-crops-fedora30   .
+#podman build  --volume "/home/ry/git_yocto/rnd-poky:/workdir"   -t ry-poky   .
