54b6a7537bc262062e8a125bd993c01fd0198dd6
maxchill <maxchill@users.noreply.github.com>
2019-12-11 09:03:12 -0500

    Update build_podman.sh

--- build_podman.sh
+++ build_podman.sh
@@ -2,5 +2,5 @@
 #podman build  -t ry-poky   .
 #-t ry-crops-fedora30
 
-podman build -f Dockerfile_fedora --volume "/home/ry/git_yocto/rnd-poky:/workdir"   -t ry-crops-fedora30   .
-#podman build  --volume "/home/ry/git_yocto/rnd-poky:/workdir"   -t ry-poky   .
+podman build -f Dockerfile_fedora -t ry-crops-fedora30   .
+
